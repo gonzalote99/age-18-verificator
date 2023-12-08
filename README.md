c6b6043915948022aaf8d0a2e451eef9a9c932db
gonzalote99 <62606335+gonzalote99@users.noreply.github.com>
2023-12-08 14:02:25 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# age-18-verificator
+
+[Edit in Codeflow ⚡️](https://stackblitz.com/~/github.com/gonzalote99/age-18-verificator)
---
+++ script.js
@@ -0,0 +1,32 @@
+const form = document.getElementById('form');
+const dob = document.getElementById('dob');
+const result = document.getElementById('result');
+
+const today = new Date();
+
+const HTMLDateFormat = today.toISOString().slice(0, 10);
+dob.setAttribute('max', HTMLDateFormat);
+
+form.addEventListener('submit', (event) => {
+ event.preventDefault();
+
+ const formData = new FormData(form);
+ const userInput = Object.fromEntries(formData);
+ const userDOB = new Date(userInput.dob);
+
+ verifyAge(userDOB);
+})
+
+
+function verifyAge(dateOfBirth) {
+const msAYear = 1000 * 60 * 60 * 24 * 365.25;
+let userAge = (today.setUTCHours(23, 59, 59, 59) - dateOfBirth) / msAYear;
+
+if(userAge < 18) {
+result.innerText = 'not +18'
+} else {
+  result.innerText = '+18'
+}
+
+
+}
